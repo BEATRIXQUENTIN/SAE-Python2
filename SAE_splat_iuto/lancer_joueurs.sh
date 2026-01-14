3c422eb336d48622a30b4a03cdb6acd37b43886e
LOUVET-Damien <damien.louvet@etu.univ-orleans.fr>
2026-01-14 17:47:22 +0100

    dernier commit

--- SAE_splat_iuto/lancer_joueurs.sh
+++ SAE_splat_iuto/lancer_joueurs.sh
@@ -1,5 +1,5 @@
 python3 -m affichage.affichage&
 python3 -m bot_ia.client_joueur --equipe LeBot&
-python3 -m bot_ia.client_joueur --equipe LeBot3&
-python3 -m bot_ia.client_joueur --equipe LeBot2&
+python3 -m bot_ia.client_joueur2 --equipe IA&
+python3 -m bot_ia.client_joueur_random --equipe Random&
 python3 -m bot_ia.client_joueur_random --equipe Random&
