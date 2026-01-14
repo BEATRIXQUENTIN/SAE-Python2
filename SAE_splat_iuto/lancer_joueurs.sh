65a432ddf1e79c232162b41d317bd7272aaeeb72
LOUVET-Damien <damien.louvet@etu.univ-orleans.fr>
2026-01-14 14:05:27 +0100

    client joueur en random

--- SAE_splat_iuto/lancer_joueurs.sh
+++ SAE_splat_iuto/lancer_joueurs.sh
@@ -1,5 +1,5 @@
 python3 -m affichage.affichage&
 python3 -m bot_ia.client_joueur --equipe joueur1&
-python3 -m bot_ia.client_joueur --equipe joueur2&
+python3 -m bot_ia.client_joueur_random --equipe Random&
 python3 -m bot_ia.client_joueur --equipe joueur3&
 python3 -m bot_ia.client_joueur --equipe joueur4&
