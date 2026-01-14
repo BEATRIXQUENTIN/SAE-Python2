7148421499123df07780da768e22149c764953c8
LOUVET-Damien <damien.louvet@etu.univ-orleans.fr>
2026-01-14 15:17:24 +0100

    joueurs

--- SAE_splat_iuto/lancer_joueurs.sh
+++ SAE_splat_iuto/lancer_joueurs.sh
@@ -1,5 +1,5 @@
 python3 -m affichage.affichage&
-python3 -m bot_ia.client_joueur --equipe joueur1&
+python3 -m bot_ia.client_joueur --equipe LeBot&
+python3 -m bot_ia.client_joueur --equipe LeBot3&
+python3 -m bot_ia.client_joueur --equipe LeBot2&
 python3 -m bot_ia.client_joueur_random --equipe Random&
-python3 -m bot_ia.client_joueur --equipe joueur3&
-python3 -m bot_ia.client_joueur --equipe joueur4&
