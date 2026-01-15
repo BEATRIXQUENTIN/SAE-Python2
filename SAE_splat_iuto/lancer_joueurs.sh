682fa638bb9b3c8df2c18ee673777d6fc001667c
LOUVET-Damien <damien.louvet@etu.univ-orleans.fr>
2026-01-15 09:58:37 +0100

    Modif lancer joueur

--- SAE_splat_iuto/lancer_joueurs.sh
+++ SAE_splat_iuto/lancer_joueurs.sh
@@ -1,5 +1,5 @@
 python3 -m affichage.affichage&
-python3 -m bot_ia.client_joueur --equipe LeBot&
-python3 -m bot_ia.client_joueur2 --equipe IA&
-python3 -m bot_ia.client_joueur_random --equipe Random&
+python3 -m bot_ia.client_joueur --equipe LeBotV2&
+python3 -m bot_ia.client_joueur_avant --equipe LeBot&
+python3 -m bot_ia.client_joueur2 --equipe LeBot3&
 python3 -m bot_ia.client_joueur_random --equipe Random&
